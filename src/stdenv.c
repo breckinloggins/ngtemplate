ee8c7abc388d000d7dfd05154556fe8cbc42b171
breckinloggins <bloggins@Breckins-MacBook-Pro-2.local>
2009-12-21 20:40:06 -0600

    Supporting :pre_escape, :xml_escape, :url_query_escape, and :cleanse_css

--- src/stdenv.c
+++ src/stdenv.c
@@ -40,7 +40,7 @@ void _mod_cstring_escape(const char* name, const char* args, const char* marker,
 }
 
 /**
- * :html_escape - Turns spaces into &nbsp; ampersands into &amp;, and so forth
+ * :html_escape - Turns ampersands into &amp;, and so forth
  */
 void _mod_html_escape(const char* name, const char* args, const char* marker, const char* value, stringbuilder* out_sb)	{
 	char* ptr;
@@ -49,7 +49,6 @@ void _mod_html_escape(const char* name, const char* args, const char* marker, co
 	while (ptr && *ptr)	{
 		switch(*ptr)	{
 			/* TODO: What to do about &nbsp;? */
-			case '\"': 	sb_append_str(out_sb, "&quot;");		break;
 			case '&':	sb_append_str(out_sb, "&amp;");			break;
 			case '<':	sb_append_str(out_sb, "&lt;");			break;
 			case '>':	sb_append_str(out_sb, "&gt;");			break;
@@ -60,6 +59,87 @@ void _mod_html_escape(const char* name, const char* args, const char* marker, co
 		ptr++;
 	}
 }
+
+
+/**
+ * :xml_escape - Turns spaces into &nbsp; ampersands into &amp;, and so forth
+ */
+void _mod_xml_escape(const char* name, const char* args, const char* marker, const char* value, stringbuilder* out_sb)	{
+	char* ptr;
+	
+	ptr = (char*)value;
+	while (ptr && *ptr)	{
+		switch(*ptr)	{
+			case '\"': 	sb_append_str(out_sb, "&quot;");		break;
+			case '\'':	sb_append_str(out_sb, "&#39;");			break;
+			case '&':	sb_append_str(out_sb, "&amp;");			break;
+			case '<':	sb_append_str(out_sb, "&lt;");			break;
+			case '>':	sb_append_str(out_sb, "&gt;");			break;
+			
+			default: sb_append_ch(out_sb, *ptr);
+		}
+		
+		ptr++;
+	}
+}
+
+/**
+ * :url_escape - Turns spaces into + and so forth
+ */
+void _mod_url_escape(const char* name, const char* args, const char* marker, const char* value, stringbuilder* out_sb)	{
+	char* ptr;
+	char escape[10];
+	ptr = (char*)value;
+	while (ptr && *ptr)	{
+		if (*ptr == ' ')	{
+			sb_append_ch(out_sb, '+');
+		} else if (isalnum(*ptr) || 
+			*ptr == '.' || 
+			*ptr == ',' || 
+			*ptr == '_' ||
+			*ptr == ':' ||
+			*ptr == '*' || 
+			*ptr == '/' ||
+			*ptr == '~' ||
+			*ptr == '!' ||
+			*ptr == '(' ||
+			*ptr == ')' ||
+			*ptr == '-')	{
+			
+			sb_append_ch(out_sb, *ptr);
+		} else {
+			snprintf(escape, 10, "%%%d", *ptr);
+			sb_append_str(out_sb, escape);
+		}
+		ptr++;
+	}
+}
+
+/**
+ * :css_cleanse - Removes characters that are not valid in CSS values
+ */
+void _mod_css_cleanse(const char* name, const char* args, const char* marker, const char* value, stringbuilder* out_sb)	{
+	char* ptr;
+	
+	ptr = (char*)value;
+	while (ptr && *ptr)	{
+		if (isalnum(*ptr) || 
+			*ptr == ' ' ||
+			*ptr == '_' ||
+			*ptr == '.' ||
+			*ptr == ',' ||
+			*ptr == '!' ||
+			*ptr == '#' ||
+			*ptr == '%' ||
+			*ptr == '-')	{
+				
+			sb_append_ch(out_sb, *ptr);
+		}
+		
+		ptr++;
+	}
+}
+
 
 /**
  * Sets up the ngtemplate global dictionary with standard values
@@ -75,6 +155,19 @@ void _init_global_dictionary(ngt_dictionary* d)	{
 void _init_standard_callbacks(ngt_template* tpl)	{
 	ngt_add_modifier(tpl, "none", _mod_none);
 	ngt_add_modifier(tpl, "cstring_escape", _mod_cstring_escape);
+	
+	// Pre and HTML escapes are currently the same since both preserve whitespace
 	ngt_add_modifier(tpl, "html_escape", _mod_html_escape);
 	ngt_add_modifier(tpl, "h", _mod_html_escape);
+	ngt_add_modifier(tpl, "pre_escape", _mod_html_escape);
+	ngt_add_modifier(tpl, "p", _mod_html_escape);
+	
+	ngt_add_modifier(tpl, "xml_escape", _mod_xml_escape);
+	ngt_add_modifier(tpl, "x", _mod_xml_escape);
+	
+	ngt_add_modifier(tpl, "url_query_escape", _mod_url_escape);
+	ngt_add_modifier(tpl, "u", _mod_url_escape);
+	
+	ngt_add_modifier(tpl, "css_cleanse", _mod_css_cleanse);
+	ngt_add_modifier(tpl, "c", _mod_css_cleanse);	
 }
--- tests/template_12.tst
+++ tests/template_12.tst
@@ -2,7 +2,15 @@
 {{!#
 CString=This is an example of a "CString" and should be 'escaped'
 HTML=There are "many" things in HTML that should be escaped, like < & >
+URL=http://www.somewhere.com/~somebody/this will need to be escaped!
+CSS=This_Is_(NOT)_A/Safe"Value"?!
 #!}}
 {{CString:cstring_escape}}
 {{HTML:html_escape}}
 {{HTML:h}}
+{{HTML:pre_escape}}
+{{HTML:p}}
+{{URL:url_query_escape}}
+{{URL:u}}
+{{CSS:css_cleanse}}
+{{CSS:c}}
